c01fefa94c1c79c5bc493e0d443fa3897825690d
Max Felius <maxfelius@hotmail.com>
2020-08-05 09:54:22 +0200

    Added some comments

--- Mandelbrot.py
+++ Mandelbrot.py
@@ -1,8 +1,7 @@
 '''
 @author: Max
 
-Script for reproducing the mandelbrot
-
+Simpel script for creating the mandelbrot figure.
 '''
 
 #imports
@@ -11,53 +10,61 @@ import matplotlib.pyplot as plt
 import time
 
 class Mandelbrot:
-	def __init__(self,xmax=1.5,xmin=-2.5,ymax=1.5j,ymin=-1.5j,step=0.01):
-		#initial variables
-		self.xmax = xmax
-		self.xmin = xmin
-		self.ymax = ymax
-		self.ymin = ymin
-		self.step = step
+    def __init__(self,xmax=1.5,xmin=-2.5,ymax=1.5j,ymin=-1.5j,step=0.01):
+        #initial variables
+        self.xmax = xmax
+        self.xmin = xmin
+        self.ymax = ymax
+        self.ymin = ymin
+        self.step = step
+        self.maxiter = 100
 
-		self.maxiter = 100
+        #compute iters and create figure
+        self.build_plot()
 
-		self.build_plot()
+    def test_number(self,c):
+        '''
+        Testing the number of steps (iterations) required to reach a modulus >= 2
+        '''
+        z = 0.0j
 
-	def test_number(self,c):
-		z = 0.0j
+        for i in range(self.maxiter):
+            z = z*z + c
+            if np.sqrt((z.real*z.real)+(z.imag*z.imag)) >= 2:
+                return i
+        return self.maxiter
 
-		for i in range(self.maxiter):
-			z = z*z + c
-			if np.sqrt((z.real*z.real)+(z.imag*z.imag)) >= 2:
-				return i
-		return self.maxiter
+    def build_plot(self):
+        '''
+        Creating the Mandelbrot Set
+        '''
 
-	def build_plot(self):
-		start = time.time()
+        #start timer
+        start = time.time()
 
-		nx = int((self.xmax-self.xmin)/self.step)
-		ny = int(((self.ymax-self.ymin)/self.step *-1j).real)
-		m = np.zeros((ny,nx,3))
+        nx = int((self.xmax-self.xmin)/self.step)
+        ny = int(((self.ymax-self.ymin)/self.step *-1j).real)
+        m = np.zeros((ny,nx,3))
 
-		for iy in range(ny):
-			y_val = self.ymax - iy*self.step*1j
-			for ix in range(nx):
-				x_val = self.xmin + ix*self.step
+        for iy in range(ny):
+            y_val = self.ymax - iy*self.step*1j
+            for ix in range(nx):
+                x_val = self.xmin + ix*self.step
 
-				c = x_val+y_val
+                c = x_val+y_val
 
-				m[iy,ix,0] = c.real
-				m[iy,ix,0] = c.imag
-				m[iy,ix,2] = self.test_number(c)
+                m[iy,ix,0] = c.real
+                m[iy,ix,0] = c.imag
+                m[iy,ix,2] = self.test_number(c)
 
-		print(f'Elapsed time for step size = {self.step} is {time.time()-start} seconds...')
+        print(f'Elapsed time for step size = {self.step} is {time.time()-start} seconds...')
 
-		plt.figure()
-		plt.imshow(m[:,:,2],cmap='hot',extent=[self.xmin,self.xmax,(self.ymin*-1j).real,(self.ymax*-1j).real])
-		plt.ylabel('Im')
-		plt.xlabel('Re')
-		plt.title('Mandelbrot Plot')
-		plt.show()
+        plt.figure()
+        plt.imshow(m[:,:,2],cmap='hot',extent=[self.xmin,self.xmax,(self.ymin*-1j).real,(self.ymax*-1j).real])
+        plt.ylabel('Im')
+        plt.xlabel('Re')
+        plt.title('Mandelbrot Plot')
+        plt.show()
 
 
 
---
+++ README.md
@@ -0,0 +1,3 @@
+# Mandelbrot Set
+
+This is a simple script for showing how to create a Mandelbrot set using Python
